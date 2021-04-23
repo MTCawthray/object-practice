07033dfabf2797651f3f3fa1207d6987f0d1c297
Mark Cawthray <dr.cawthray@gmail.com>
2021-04-23 10:35:41 -0600

    Add car object

--- object-examples.js
+++ object-examples.js
@@ -4,3 +4,10 @@ var weinerDog = {
   coatColor: "blonde",
   names: ["Rudder", "Howy Weiners", "Ruddy Buddy", "Rhubarb"]
 }
+
+var car = {
+  make: "Subaru",
+  model: "Outback",
+  year: 2010,
+  workDone: ["changed oil", "new shift sensor", "new timing belt"]
+}
